720d32ce59d178e5847e2a495fe34439fcda3754
Greg Kittleson <greg.kittleson@gmail.com>
2019-01-29 11:39:41 -0800

    Instantiate express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+var express = require('express');
+const port = 8000;
 
+var app = express();
 
+app.use(express.static(__dirname));
+app.listen(port, err => {
+  if (err) {
+    console.error('Error :', err);
+  } else {
+    console.log('Listening at Port:', port);
+  }
+});

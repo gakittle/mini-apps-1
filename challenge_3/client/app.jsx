7968a3ce7110935012d3b874692d8fbb30446650
Greg Kittleson <greg.kittleson@gmail.com>
2019-01-30 15:22:25 -0800

    Render form components for all input pages

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,18 +2,95 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 'home'
+      page: 'home',
       // 'home', 'f1', 'f2', 'f3'
+      f1: ['Name', 'Email', 'Password'],
+      f2: [
+        'Address Line 1',
+        'Address Line 2',
+        'City',
+        'State',
+        'ZIP Code',
+        'Phone #'
+      ],
+      f3: [
+        'Credit Card #',
+        'Expiry Date',
+        'CVV',
+        'Billing Address 1',
+        'Billing Address 2',
+        'City',
+        'State',
+        'ZIP Code'
+      ]
     };
   }
 
   render() {
     return (
-      <div className="checkout">
-        <button>Check Out!</button>
+      <div id="parent">
+        <Home />
+
+        <F1 fields={this.state.f1} />
+
+        <F2 fields={this.state.f2} />
+
+        <F3 fields={this.state.f3} />
       </div>
     );
   }
 }
+
+var Field = props => {
+  return (
+    <div className="info">
+      {props.title + ':'}
+      <form className="text">
+        <input />
+      </form>
+    </div>
+  );
+};
+
+var Home = props => {
+  return (
+    <div id="home">
+      <button>Check Out!</button>
+    </div>
+  );
+};
+
+var F1 = props => {
+  return (
+    <div id="f1">
+      {props.fields.map(field => {
+        return <Field title={field} />;
+      })}
+      <button className="submit">Next!</button>
+    </div>
+  );
+};
+
+var F2 = props => {
+  return (
+    <div id="f2">
+      {props.fields.map(field => {
+        return <Field title={field} />;
+      })}
+      <button className="submit">Next!</button>
+    </div>
+  );
+};
+
+var F3 = props => {
+  return (
+    <div id="f3">
+      {props.fields.map(field => {
+        return <Field title={field} />;
+      })}
+      <button className="submit">Next!</button>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));

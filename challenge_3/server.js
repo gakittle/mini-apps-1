e037b769e6832c56aa44713644802a1b200f7163
Greg Kittleson <greg.kittleson@gmail.com>
2019-01-30 12:22:24 -0800

    Implement express server w/ middleware

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,19 @@
+let express = require('express');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+const port = 3000;
 
+let app = express();
 
+app.use(express.static('./public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, err => {
+  if (err) {
+    throw err;
+  } else {
+    console.log('Listening at Port', port);
+  }
+});

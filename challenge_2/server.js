b18e25ca98c017124f08b89c719292e88a180ebf
Greg Kittleson <greg.kittleson@gmail.com>
2019-01-29 19:44:53 -0800

    Edit for code clean-up

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -12,14 +12,11 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  console.log('getting:', req.body);
   res.sendFile(__dirname + '/client/index.html');
 });
 
 app.post('/', (req, res) => {
-  console.log('Incoming request:', req.body.input);
   var csv = convertData(JSON.parse(req.body.input));
-  console.log('Imma try and send this:', csv);
   res.status(201);
   res.type('csv');
   res.send(csv);

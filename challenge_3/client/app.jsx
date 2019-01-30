b3fc583f569ddca089ea1292ac3282f247fd6c7b
Greg Kittleson <greg.kittleson@gmail.com>
2019-01-30 14:14:21 -0800

    Render App to index with imported React files

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,19 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 'home'
+      // 'home', 'f1', 'f2', 'f3'
+    };
+  }
 
+  render() {
+    return (
+      <div className="checkout">
+        <button>Check Out!</button>
+      </div>
+    );
+  }
+}
 
+ReactDOM.render(<App />, document.getElementById('app'));

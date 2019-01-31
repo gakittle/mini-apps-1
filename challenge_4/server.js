eaecb0236d990296d24a8506f9a0b7d37c7c81d9
Greg Kittleson <greg.kittleson@gmail.com>
2019-01-31 11:06:29 -0800

    Initialize express server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,18 @@
+var express = require('express');
+var parser = require('body-parser');
+var morgan = require('morgan');
+const port = 3000;
 
+var app = express();
 
+app.use(express.static('./client/dist'));
+app.use(morgan('dev'));
+app.use(parser.json());
+
+app.listen(port, err => {
+  if (err) {
+    throw err;
+  } else {
+    console.log('Listening at Port:', port);
+  }
+});
